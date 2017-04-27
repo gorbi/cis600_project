6740c9232d07dcf22405330cc168ce8c99910f91
Nagaprasad Natarajaurs <nagaprasad@outlook.in>
2017-04-27 16:32:52 -0400

    Add option to login with other accounts

--- app/src/main/java/com/mkchaudh/nnataraj/orangeftp/LoginActivity.java
+++ app/src/main/java/com/mkchaudh/nnataraj/orangeftp/LoginActivity.java
@@ -8,22 +8,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ResultCodes;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.mkchaudh.nnataraj.orangeftp.data.FirebaseHelper;
 
+import java.util.Arrays;
+
 public class LoginActivity extends AppCompatActivity {
 
     private static final String CREATE_ACCOUNT_ERROR = "Unable to create account";
     private static final String SIGN_IN_ERROR = "Unable to sign in";
+
+    private static final int REQUEST_USE_OTHER_ACCOUNT = 1;
 
     private void proceed() {
         FirebaseHelper.set(FirebaseAuth.getInstance().getCurrentUser().getUid());
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_USE_OTHER_ACCOUNT) {
+            // Successfully signed in
+            if (resultCode == ResultCodes.OK) {
+                proceed();
+            } else {
+                // Sign in failed
+                Snackbar.make(findViewById(R.id.clickHere), SIGN_IN_ERROR, Snackbar.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
@@ -79,6 +98,18 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.clickHere).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(AuthUI
+                                .getInstance()
+                                .createSignInIntentBuilder()
+                                .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                        new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
+                                .build(),
+                        REQUEST_USE_OTHER_ACCOUNT);
+            }
+        });
 
     }
 
